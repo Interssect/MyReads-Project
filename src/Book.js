00a21f414bb900f1cedd6dc04673438175626efa
Dario Pek <36820730+Interssect@users.noreply.github.com>
2018-07-31 19:40:24 +0200

    Searching error fixed
    Search failed due to missing thumbnail  'biography' was searched.

--- src/Book.js
+++ src/Book.js
@@ -2,23 +2,34 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import ShelfChanger from "./ShelfChanger";
 
+const getThumbnail = (book) => {
+	if (typeof(book.imageLinks) !== 'undefined') {
+		return book.imageLinks.smallThumbnail;} else {
+		return 'http://via.placeholder.com/128x193?text=No%20Cover'
+	}
+}
 class Book extends Component {
-	static propTypes = {book: PropTypes.object.isRequired,changeShelf: PropTypes.func.isRequired
+	static propTypes = {
+		book: PropTypes.object.isRequired,
+		changeShelf: PropTypes.func.isRequired
 	};
-    render(){
-        const { book } = this.props;
-        return(
-           <div className="book" id={book.id}>
-               <div className="book-top">
-                   <div className="book-cover" style={{backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                   <ShelfChanger
-                       book={book}
-                       changeShelf={this.props.changeShelf}/>
-               </div>
-               <div className="book-title">{book.name}</div>
-               <div className="book-authors">{book.authors}</div>
-           </div>
-        )
-    }
+	render() {
+		const {
+			book
+		} = this.props;
+		return ( < div className = "book"id = {book.id} > 
+				 < div className = "book-top" > 
+				 < div className = "book-cover" style = {
+				{
+					width: 128,
+					height: 193,
+					backgroundImage: `url(${getThumbnail(book)})`}} > 
+                 < /div> < ShelfChanger book = {book}
+			     changeShelf = {this.props.changeShelf}/> 
+                 < /div>
+                 < div className = "book-title" > {book.name} < /div> 
+                 < div className = "book-authors" > {book.authors} < /div> 
+                 < /div>)
+	}
 }
 export default Book;
